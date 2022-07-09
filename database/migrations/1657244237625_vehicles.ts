874066cac7d6fb62faf2890500b897570ccf8ff0
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-09 12:34:06 -0300

    feat(Vehicle): create model and migrations

---
+++ database/migrations/1657244237625_vehicles.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+	protected tableName = 'vehicles'
+
+	public async up () {
+		this.schema.createTable(this.tableName, (table) => {
+			table.increments('id')
+			table.string('name')
+			table.string('description')
+			table.float('price')
+			table.boolean('isFavorite')
+			table.string('color')
+			table.string('plate')
+			table.string('year')
+			table.timestamp('created_at', { useTz: true })
+			table.timestamp('updated_at', { useTz: true })
+		})
+	}
+
+	public async down () {
+		this.schema.dropTable(this.tableName)
+	}
+}
