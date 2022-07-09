c6a0370efb8fedb51f1cbf71fe875fa9c6169f9c
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-09 19:46:36 -0300

    chore: add ssl suport

--- Dockerfile
+++ Dockerfile
@@ -25,6 +25,7 @@ ENV HOST=0.0.0.0
 COPY ./package*.json ./
 RUN npm ci --production
 COPY --from=build /app/build .
+RUN npm run migration
 EXPOSE $PORT
 
-CMD [ "npm", "start" ]
+CMD [ "npm", "migration", "start" ]
--- config/database.ts
+++ config/database.ts
@@ -8,26 +8,42 @@
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
+const postgresConfig: any = {
+	host: Env.get('PG_HOST'),
+	port: Env.get('PG_PORT'),
+	user: Env.get('PG_USER'),
+	password: Env.get('PG_PASSWORD', ''),
+	database: Env.get('PG_DB_NAME'),
+}
+
+const postgresConfigWithSSL: any = {
+	...postgresConfig,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+}
+
+console.log({ VARIABLE: Env.get('DB_SSL') })
+
+console.log(Env.get('DB_SSL') === 'true')
+const connections = Env.get('DB_SSL') === 'true'
+	? postgresConfigWithSSL
+	: postgresConfig
+
+console.log({ connections })
+
 const databaseConfig: DatabaseConfig = {
 	connection: Env.get('DB_CONNECTION'),
-
 	connections: {
 		pg: {
 			client: 'pg',
-			connection: {
-				host: Env.get('PG_HOST'),
-				port: Env.get('PG_PORT'),
-				user: Env.get('PG_USER'),
-				password: Env.get('PG_PASSWORD', ''),
-				database: Env.get('PG_DB_NAME'),
-			},
+			connection: connections,
 			migrations: {
 				naturalSort: true,
 			},
 			healthCheck: false,
 			debug: false,
 		},
-
 	},
 }
 
