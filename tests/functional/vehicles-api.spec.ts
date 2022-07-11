4ba01e7718986c5cf6cd3e87112a0022d8fa442a
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-11 01:24:39 -0300

    test(VehiclesController): ensure it displays all vehicles given the search result with COLOR

--- tests/functional/vehicles-api.spec.ts
+++ tests/functional/vehicles-api.spec.ts
@@ -92,6 +92,12 @@ test.group('/vehicles/:search/search [GET]', (group) => {
 		response.assertStatus(200)
 		response.assert?.isTrue(response.body().length > 0)
 	})
+	test('should display all vehicles given the search result with COLOR', async ({ client }) => {
+		await Vehicle.create(mockVehicle())
+		const response = await client.get('/vehicles/Vermelho/search')
+		response.assertStatus(200)
+		response.assert?.isTrue(response.body().length > 0)
+	})
 })
 
 test.group('vehicles/:id [GET]', (group) => {
